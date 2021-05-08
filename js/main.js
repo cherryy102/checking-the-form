3903e7f35551a89a4da8456af0ee231d5ab00561
Damian Wiśniowski <dkontow@gmail.com>
2021-05-08 15:27:37 +0200

    form validation

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const nameError = document.querySelector('.error_name');
+const emailError = document.querySelector('.error_email');
+const passError = document.querySelector('.error_pass');
+const passCheckError = document.querySelector('.error_passCheck');
+
+const nameInput = document.querySelector('.name-input');
+const emailInput = document.querySelector('.email-input');
+const passInput = document.querySelector('.pass-input');
+const passCheckInput = document.querySelector('.passCheck-input');
+
+let send = true;
+nameInput.addEventListener('blur', () => {
+    const index = nameInput.value.indexOf(' ');
+    if (nameInput.value.length <= index + 1) {
+        nameError.textContent = 'Podaj imię i nazwisko';
+        send = false;
+    } else if (!nameInput.value.includes(' ')) {
+        nameError.textContent = 'Podaj imię i nazwisko';
+        send = false
+    } else {
+        nameError.textContent = '';
+        send = true;
+    }
+})
+
+emailInput.addEventListener('blur', () => {
+    const index = emailInput.value.indexOf('@');
+    if (!emailInput.value.includes('@') || emailInput.value.length <= index + 1) {
+        emailError.textContent = 'Niepoprawny email';
+        send = false;
+    } else {
+        emailError.textContent = '';
+        send = true;
+    }
+})
+
+passInput.addEventListener('blur', () => {
+    const regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$!%*?&)(=])([a-zA-Z0-9@#$!%*?&()=]{8,})$/;
+    if (regex.exec(passInput.value) === null) {
+        passError.textContent = 'Hasło musi zawierać duże i małe litery, cyfry, znaki oraz długość min. 8 znaków';
+        send = false;
+    } else {
+        passError.textContent = '';
+        send = true;
+    }
+})
+passInput.addEventListener('input', () => {
+    if (passCheckInput.value !== passInput.value) {
+        passCheckError.textContent = "Hasła są różne";
+        send = false
+    } else {
+        passCheckError.textContent = "";
+        send = true;
+    }
+})
